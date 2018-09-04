d6d49ff7e26c35effac1815992f5a69026401515
0x00sec Jinx <0x00sec_jinx@protonmail.ch>
2018-09-04 10:10:19 -0400

    Add basic tanh activation function

--- activation.c
+++ activation.c
@@ -5,6 +5,10 @@ float sigmoid(float x){
 	return 1 / (1 + (pow((float)M_E, (float)-x)));
 }
 
+float tanh(float y) {
+	return (pow((float)M_E, y) - pow((float)M_E, -y)) / (pow((float)M_E, y) + pow((float)M_E, -y));
+}
+
 static PyObject *activation_sigmoid(PyObject *self, PyObject *args) {
 	float x;
 	float result;
@@ -65,11 +69,38 @@ static PyObject *activation_drelu(PyObject *self, PyObject *args) {
 	
 }
 
+static PyObject *activation_tanh(PyObject *self, PyObject *args) {
+	float y;
+	float result;
+	if(!PyArg_ParseTuple(args,"f", &y)){
+		printf("tanh requires a float!\n");
+		Py_INCREF(Py_None);
+		return Py_None;
+	}
+	result = (pow((float)M_E, y) - pow((float)M_E, -y)) / (pow((float)M_E, y) + pow((float)M_E, -y));
+	return Py_BuildValue("f", result);
+	
+}
+
+static PyObject *activation_dtanh(PyObject* self, PyObject *args) {
+	float y;
+	float result;
+	if(!PyArg_ParseTuple(args,"f", &y)){
+		printf("dtanh requires a float!\n");
+		Py_INCREF(Py_None);
+		return Py_None;
+	}
+	result = 1 - pow(tanh(y), float(2));
+	return Py_BuildValue("f", result);
+}
+
 static PyMethodDef module_methods[] = {
 	{"sigmoid",  (PyCFunction)activation_sigmoid,  METH_VARARGS, NULL},
 	{"dsigmoid", (PyCFunction)activation_dsigmoid, METH_VARARGS, NULL},
 	{"relu",     (PyCFunction)activation_relu,     METH_VARARGS, NULL},
 	{"drelu",    (PyCFunction)activation_drelu,    METH_VARARGS, NULL},
+	{"tanh",     (PyCFunction)activation_tanh,     METH_VARARGS, NULL},
+	{"dtanh",    (PyCFunction)activation_dtanh,    METH_VARARGS, NULL},
 	{NULL}
 };
 
